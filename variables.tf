68035e24a191d1f873458e4b0709a5089a3ad76f
SantuDGithub <121543660+SantuDGithub@users.noreply.github.com>
2023-03-01 13:10:43 +0530

    Delete variables.tf

--- variables.tf
+++
@@ -1,9 +0,0 @@
-variable "access_key" {
-  default = "AKIAZXWFLSBHE2RF5ANN"
-  }
-variable "secret_key" {
-  default = "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"
-  }
-variable "region" {
-  default = "ap-south-1"
-  }
